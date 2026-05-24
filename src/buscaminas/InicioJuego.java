8359cac9a8ffd6b89a99a3834035c9aee2249e8b
londonlui <luisa.londono.nwn@gmail.com>
2026-05-23 19:33:31 -0500

    Update InicioJuego.java

--- src/buscaminas/InicioJuego.java
+++ src/buscaminas/InicioJuego.java
@@ -1,1 +1,45 @@
 package buscaminas;
+
+import java.util.Scanner;
+
+public class InicioJuego{
+  
+  private Scanner scanner;
+  private Registro registro;
+  private Tablero tablero;
+
+  public InicioJuego(){
+    this.scanner = new Scanner(System.in);
+    this.registro = new Registro();
+    this.tablero=null;
+  }
+
+  public void iniciar(){
+    
+    boolean sesionActiva = true;
+    while(sesionActiva){
+      mostrarMenu();
+      String opcion = scanner.nextLine().trim();
+      switch(opcion){
+        case "1":
+          jugarPartida();
+          break;
+        case "2":
+        case "Historial":
+          registro.mostrar(scanner);
+          break;
+        case "3":
+          System.out.println();
+          System.out.printl("Opción no válida. Elige 1, 2, 3.");
+      }
+    }
+    scanner.close();
+  }
+
+  private void mostrarMenu(){
+    
+          
+          
+      
+
+  
