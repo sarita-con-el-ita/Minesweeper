4baf60478da2e5df7ea05ff9aec4c64f6f3190ae
londonlui <luisa.londono.nwn@gmail.com>
2026-05-23 20:24:58 -0500

    Update InicioJuego.java

--- src/buscaminas/InicioJuego.java
+++ src/buscaminas/InicioJuego.java
@@ -40,6 +40,66 @@ public class InicioJuego{
     System.out.println("+--------------------------+");
     System.out.println("|      Menú principal      |");
     System.out.println("+--------------------------+");
+    System.out.println("| 1. Nueva partida         |");
+    System.out.println("| 2. Historial             |");
+    System.out.println("| 3. Salir                 |");
+    System.out.println("+--------------------------+");
+    System.out.println("| Elige una opción         |");
+  }
+
+  private void elegirNivel(){
+    System.out.println();
+    System.out.println("+--------------------------+");
+    System.out.println("|   Nivel de dificultad    |");
+    System.out.println("+--------------------------+");
+    System.out.println("| 1.Fácil:(9x9, 10 minas)  |");
+    System.out.println("| 2.Medio:(16x16, 40 minas |");
+    System.out.println("| 3.Difícil:(16x30, 99minas|");
+    System.out.println("| 4.Personalizado          |");
+    System.out.println("+--------------------------+");
+    System.out.println(" Elige el nivel ");
+
+    String linea = scanner.nextLine.trim();
+    switch(linea){
+      case "1": tablero = new Tablero(Tablero.FACIL);
+        break;
+      case "2": tablero = new Tablero(Tablero.MEDIO);
+        break;
+      case "3": tablero = new Tablero(Tablero.DIFICIL);
+        break;
+      case "4": tablero = crearPersonalizado();
+        break;
+      default:
+        System.out.println(" Opción no válida. Se usará fácil");
+        tablero = new Tablero(Tablero.FACIL);
+    }
+  }
+
+  private void crearPersonalizado(){
+    System.out.println();
+    System.out.println("Advertencia: El tablero debe ser mínimo 2x2.");
+    System.out.println();
+    int filas = leerEnteroEnRango("Filas (mínimo 2):", 2 , 100);
+    int columnas = leerEnteroEnRango("Columnas(mínimo2):", 2, 100);
+    int maxMinas = filas * columnas-1;
+    int minas = leerEnteroEnRango("Minas (max " + maxMinas + "): ", 1, maxMinas);
+    tablero = new Tablero(filas, columnas, minas);
+  }
+
+  private void jugarPartida(){
+    elegirNivel();
+
+    long inicio = System.currentMillis();
+    boolean termino = false;
+    boolean gano = false;
+    boolean salio = false;
+
+    System.out.println();
+    System.out.println(" Nivel: " + tablero.getNombreNivel() + " | Casillas del " + tablero.getNumeroBase() + " al " + tablero.getUltimoNumero());
+    System.out.println()
+
+    
+    
     
     
           
